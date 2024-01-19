aa246c46c1e50b7bb086ca12e9c04708ddb834b0
Heathcliff <heathcliff@heathcliff.eu>
2024-01-19 14:44:48 +0100

    CI: Use new ci repo instead of duplicating workflows locally
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/coverprofile.sh
+++ hack/coverprofile.sh
@@ -7,12 +7,13 @@ script_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 pushd "${script_dir}" >/dev/null
 
 OUT_DIR="${script_dir}/coverprofiles"
+APP="promremote"
 
 if [ ! -d "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-go test -coverprofile="${OUT_DIR}/cover-promremote.out" "./..."
-go tool cover -html "${OUT_DIR}/cover-promremote.out" -o "${OUT_DIR}/index.html"
+go test -coverprofile="${OUT_DIR}/cover-${APP}.out" "./..."
+go tool cover -html "${OUT_DIR}/cover-${APP}.out" -o "${OUT_DIR}/index.html"W
 
 popd >/dev/null
