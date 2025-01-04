4b83b09c2a9a15daf16ff07851a8f2ad6d55a089
Heathcliff <heathcliff@heathcliff.eu>
2025-01-04 11:33:15 +0100

    Add unit-test for Run method and other improvements
    Improve unit-test for errors.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- promremote/errors_test.go
+++ promremote/errors_test.go
@@ -2,16 +2,31 @@ package promremote
 
 import (
 	"io"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewErrRemoteWriteFailed(t *testing.T) {
-	result := &ErrRemoteWriteFailed{StatusCode: 400, Body: "testresult"}
-	r := io.NopCloser(strings.NewReader(result.Body))
-	defer r.Close()
-	err := NewErrRemoteWriteFailed(400, r)
-	assert.Equal(t, result, err)
+func TestErrRemoteWriteFailed(t *testing.T) {
+	t.Run("New", func(t *testing.T) {
+		assert := assert.New(t)
+
+		expectedResult := &ErrRemoteWriteFailed{StatusCode: 400, Body: "testresult"}
+
+		r := io.NopCloser(strings.NewReader(expectedResult.Body))
+		defer r.Close()
+		result := NewErrRemoteWriteFailed(400, r).(*ErrRemoteWriteFailed)
+		assert.Equal(expectedResult, result, "Should read the response body")
+
+		f, err := os.Open("/dev/null")
+		if !assert.NoError(err, "Should not fail to open /dev/null") {
+			t.FailNow()
+		}
+		f.Close()
+		result = NewErrRemoteWriteFailed(400, f).(*ErrRemoteWriteFailed)
+		assert.Contains(result.Body, "file already closed", "Should return error of reader as body")
+	})
+
 }
