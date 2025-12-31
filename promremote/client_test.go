8640285afeae779dcdc87f785419ce3f402576cc
Heathcliff <heathcliff@heathcliff.eu>
2025-12-31 17:06:16 +0100

    Use client_golang remote api
    Instead of crafting the requests manually, use the new remote write api from
    client_golang.
    This drops the dependency on prometheus/prometheus and implements remote write
    v2.
    
    Closes: #78
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- promremote/client_test.go
+++ promremote/client_test.go
@@ -2,19 +2,15 @@ package promremote
 
 import (
 	"bytes"
-	"encoding/base64"
 	"log"
-	"net/http"
-	"net/http/httptest"
 	"os"
-	"reflect"
 	"testing"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
-	"github.com/prometheus/prometheus/prompb"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewWriteClient(t *testing.T) {
@@ -23,25 +19,20 @@ func TestNewWriteClient(t *testing.T) {
 		Registry                      *prometheus.Registry
 		Error                         string
 	}{
-		{"MissingEndpoint", "", "testinstance", "testjob", prometheus.NewRegistry(), "promremote.ErrMissingEndpoint"},
-		{"MissingInstance", "test-endpoint", "", "testjob", prometheus.NewRegistry(), "promremote.ErrMissingInstance"},
-		{"MissingJob", "test-endpoint", "testinstance", "", prometheus.NewRegistry(), "promremote.ErrMissingJob"},
-		{"MissingRegistry", "test-endpoint", "testinstance", "testjob", nil, "promremote.ErrMissingRegistry"},
+		{"MissingEndpoint", "", "testinstance", "testjob", prometheus.NewRegistry(), "No endpoint for prometheus remote_write provided"},
+		{"MissingInstance", "test-endpoint", "", "testjob", prometheus.NewRegistry(), "No instance name provided"},
+		{"MissingJob", "test-endpoint", "testinstance", "", prometheus.NewRegistry(), "No job name provided"},
+		{"MissingRegistry", "test-endpoint", "testinstance", "testjob", nil, "No prometheus registry provided"},
 	}
 
 	for _, tCase := range tMatrix {
 		t.Run(tCase.Name, func(t *testing.T) {
 			c, err := NewWriteClient(tCase.Endpoint, tCase.Instance, tCase.Job, tCase.Registry)
 
-			assert := assert.New(t)
+			require := require.New(t)
 
-			assert.Nil(c)
-			if !assert.Error(err) {
-				t.Fatal("Did not receive an error")
-			}
-			if !assert.Equal(tCase.Error, reflect.TypeOf(err).String()) {
-				t.Fatalf("Received invalid error: %v", err)
-			}
+			require.Nil(c, "Should not return a client")
+			require.ErrorContains(err, tCase.Error, "Should return the correct error")
 		})
 	}
 
@@ -50,8 +41,8 @@ func TestNewWriteClient(t *testing.T) {
 
 		assert := assert.New(t)
 
-		assert.Nil(err)
-		assert.NotEmpty(c)
+		assert.NoError(err, "should not return an error")
+		assert.NotEmpty(c, "Should return a client")
 	})
 }
 
@@ -62,9 +53,10 @@ func TestClientGet(t *testing.T) {
 		assert := assert.New(t)
 
 		res := c.Endpoint()
-		assert.NotEmpty(res)
-		res = cNil.Endpoint()
-		assert.Empty(res)
+		assert.NotEmpty(res, "Should return endpoint")
+		assert.NotPanics(func() {
+			assert.Empty(cNil.Endpoint(), "Should return an empty string")
+		}, "Should not panic when called on nil client")
 	})
 
 	t.Run("Registry", func(t *testing.T) {
@@ -74,6 +66,9 @@ func TestClientGet(t *testing.T) {
 		assert.NotEmpty(res)
 		res = cNil.Registry()
 		assert.Empty(res)
+		assert.NotPanics(func() {
+			assert.Nil(cNil.Registry(), "Should return no registry")
+		}, "Should not panic when called on nil client")
 	})
 }
 
@@ -82,73 +77,25 @@ func TestClientSetBasicAuth(t *testing.T) {
 
 	tMatrix := []struct {
 		Username, Password string
-		Error              error
+		ShouldError        bool
 	}{
-		{"testuser", "password", nil},
-		{"testuser", "", ErrMissingAuthCredentials{}},
-		{"", "password", ErrMissingAuthCredentials{}},
-		{"", "", ErrMissingAuthCredentials{}},
+		{"testuser", "password", false},
+		{"testuser", "", true},
+		{"", "password", true},
+		{"", "", true},
 	}
 
 	assert := assert.New(t)
 
 	for _, tCase := range tMatrix {
 		err := c.SetBasicAuth(tCase.Username, tCase.Password)
-		if tCase.Error == nil {
-			assert.Nil(err)
-			assert.Equal(tCase.Username, c.username)
-			assert.Equal(tCase.Password, c.password)
+		if tCase.ShouldError {
+			assert.ErrorContains(err, "Need both username and password, at least one of them is empty", "Should return error")
 		} else {
-			assert.Equal(tCase.Error, err)
+			assert.NoError(err, "Should not return an error")
+			assert.Equal(tCase.Username, c.username, "Username should be set")
+			assert.Equal(tCase.Password, c.password, "Password should be set")
 		}
-	}
-}
-
-func TestPost(t *testing.T) {
-	tMatrix := []struct {
-		Name   string
-		Client *Client
-		Auth   bool
-	}{
-		{
-			Name:   "WithoutAuth",
-			Client: &Client{},
-			Auth:   false,
-		},
-		{
-			Name: "WithAuth",
-			Client: &Client{
-				username: "testuser",
-				password: "password",
-			},
-			Auth: true,
-		},
-	}
-
-	for _, tCase := range tMatrix {
-		t.Run(tCase.Name, func(t *testing.T) {
-			assert := assert.New(t)
-
-			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-				assert.Equal(http.MethodPost, req.Method)
-
-				assert.Equal("snappy", req.Header.Get("Content-Encoding"))
-				assert.Equal("application/x-protobuf", req.Header.Get("Content-Type"))
-				assert.Equal("0.1.0", req.Header.Get("X-Prometheus-Remote-Read-Version"))
-				if tCase.Auth {
-					auth := req.Header.Get("Authorization")
-					expectedAuth := "Basic " + base64.StdEncoding.EncodeToString([]byte(tCase.Client.username+":"+tCase.Client.password))
-					assert.Equal(expectedAuth, auth)
-				}
-
-				_, _ = rw.Write([]byte("Success"))
-			}))
-			defer server.Close()
-
-			tCase.Client.endpoint = server.URL
-			err := tCase.Client.post([]prompb.TimeSeries{})
-			assert.Nil(err)
-		})
 	}
 }
 
@@ -159,11 +106,57 @@ func TestCollect(t *testing.T) {
 	c, _ := NewWriteClient("testendpoint", "test", "test", reg)
 
 	assert := assert.New(t)
+	require := require.New(t)
 
-	ts, err := c.collect()
+	req, err := c.collect()
 
-	assert.Nil(err)
-	assert.NotEmpty(ts)
+	require.NoError(err, "Should collect metrics without error")
+	require.NotEmpty(req, "Should return a remote write request")
+	require.NotEmpty(req.Timeseries, "Should have collected some metrics")
+
+	assert.NotEmpty(req.Symbols, "Should have a symbol table")
+	ts := req.Timeseries[0]
+	assert.NotEmpty(ts.Metadata, "TimeSeries should have metadata")
+	assert.NotEmpty(ts.LabelsRefs, "TimeSeries should have label references")
+	assert.NotEmpty(ts.Samples, "TimeSeries should have samples")
+}
+
+func TestUrl(t *testing.T) {
+	t.Run("BasicURL", func(t *testing.T) {
+		assert := assert.New(t)
+
+		c := &Client{
+			endpoint: "http://prometheus.example.com:1234/test/write",
+		}
+		url, err := c.url()
+
+		assert.NoError(err, "Should not return an error")
+		assert.Equal(c.Endpoint(), url.String(), "Should return correct URL")
+	})
+	t.Run("WithBasicAuth", func(t *testing.T) {
+		assert := assert.New(t)
+
+		c := &Client{
+			endpoint: "http://prometheus.example.com:1234/test/write",
+			username: "testuser",
+			password: "testpassword",
+		}
+		url, err := c.url()
+
+		assert.NoError(err, "Should not return an error")
+		assert.Equal("http://testuser:testpassword@prometheus.example.com:1234/test/write", url.String(), "Should return correct URL")
+	})
+	t.Run("ParseError", func(t *testing.T) {
+		assert := assert.New(t)
+
+		c := &Client{
+			endpoint: "%",
+		}
+		url, err := c.url()
+
+		assert.Error(err, "Should return an error")
+		assert.Nil(url, "Should not return a URL")
+	})
 }
 
 func TestRun(t *testing.T) {
@@ -178,7 +171,8 @@ func TestRun(t *testing.T) {
 	log.SetOutput(&buf)
 
 	quit := make(chan bool)
-	c.Run(time.Second, quit)
+	err := c.Run(time.Second, quit)
+	assert.NoError(err, "Should not return an error")
 
 	<-time.After(time.Second * 2)
 	quit <- true
